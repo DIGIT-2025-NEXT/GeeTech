a73bb02569a5ca723d06080ba05d12c50001f21f
Ferrarin9238 <aaaaa2828kugi@gmail.com>
2025-09-05 14:00:52 +0900

    creat login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,21 @@
+import { Box, Button, Container, TextField, Typography } from '@mui/material';
+import Link from 'next/link';
+
+export default function Login() {
+    return (
+        <Container maxWidth="sm">
+            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+                <Typography variant="h4">Login</Typography>
+            </Box>
+            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+                <TextField label="Email" type="email" />
+                <TextField label="Password" type="password" />
+                <Button variant="contained" color="primary">Login</Button>
+            </Box>
+            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+                <Typography variant="body1">Don't have an account? <Link href="/register">Register</Link></Typography>
+            </Box>
+        </Container>
+    )
+}
+
