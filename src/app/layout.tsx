afdc97cc7a9466c8420a75a1cb01317b34172f86
PolyChromaticLithic <PolyChromaticLithic@outlook.jp>
2025-09-05 16:37:35 +0900

    HTML lang="en"になっていたのでjpに修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="jp">
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${notoSansJp.variable} ${audiowide.variable} font-sans antialiased`}
       >
