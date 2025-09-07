4d6dad0017bf2b9e2d8e07550431513856e4bed2
Ferrarin9238 <aaaaa2828kugi@gmail.com>
2025-09-07 12:14:16 +0900

    chat/[chatid]のUIとchatのmockdataの作成

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,48 +1,26 @@
 
-import { Box, Button, Card, CardActions, CardContent, Container, Stack, TextField, Typography} from '@mui/material';
+import { Box, Button, Card, CardActions, CardContent, Container, Stack, TextField, Typography,Breadcrumbs} from '@mui/material';
 import Link from 'next/link';
+import { getAllChat, type Chat } from '@/lib/mock';
 
 export default function Chat() {
+  const chats=getAllChat;
     return (
-        <Container maxWidth="sm">
-            <Typography>chat一覧</Typography>
+        <Container maxWidth="xl" sx={{ py: 4 }}>
+        <Breadcrumbs sx={{ mb: 2 }}>
+          <Link href="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+            ホーム
+          </Link>
+          <Typography color="text.primary">チャット一覧</Typography>
+        </Breadcrumbs>
             <Stack spacing={2}>
-            <Card>
+              <Card>
               <CardContent>
                 <Typography>chat1</Typography>
                 <Button href='/chat/[chatid]'>chatid</Button>
               </CardContent>
             </Card>
-            <Card>
-              <CardContent>
-                <Typography>chat1</Typography>
-                <Button href='/chat/[chatid]'>chatid</Button>
-              </CardContent>
-            </Card>
-            <Card>
-              <CardContent>
-                <Typography>chat1</Typography>
-                <Button href='/chat/[chatid]'>chatid</Button>
-              </CardContent>
-            </Card>
-            <Card>
-              <CardContent>
-                <Typography>chat1</Typography>
-                <Button href='/chat/[chatid]'>chatid</Button>
-              </CardContent>
-            </Card>
-            <Card>
-              <CardContent>
-                <Typography>chat1</Typography>
-                <Button href='/chat/[chatid]'>chatid</Button>
-              </CardContent>
-            </Card>
-            <Card>
-              <CardContent>
-                <Typography>chat1</Typography>
-                <Button href='/chat/[chatid]'>chatid</Button>
-              </CardContent>
-            </Card>
+            
         </Stack>
         </Container>
     )
